9e49016e1e85f71b9f9582f4c9ae478f1cada477
unknown <abcd132418@gmail.com>
2020-12-01 19:22:50 +0800

    test-06

--- midterm/blog/tests.py
+++ midterm/blog/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class PostingTest(TestCase):
+    def test_user_posting(self):
+        c = Client()
+
+        response = c.post('http://127.0.0.1:8000/blog/post/',
+                          {'title': 'hello blog', 'content': 'Posting Test'})
+        self.assertEqual(Post.objects.count(), 1)
+
+    def tearDown(self):
+        post = Post.objects.get(title='hello blog')
+        post.delete()
+
+
+class ModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title='hello blog', content='Model Test')
+
+    def test_model(self):
+        t = Post.objects.count()
+        self.assertEqual(t, 1)
+
+    def tearDown(self):
+        post = Post.objects.get(title='hello blog')
+        post.delete()
