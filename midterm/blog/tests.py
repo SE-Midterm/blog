cee5866c67ffd52ae06105944132481ced8ef0b6
ArthurLiao0816 <s110710529@student.nqu.edu.tw>
2020-12-01 20:13:21 +0800

    test-29

--- midterm/blog/tests.py
+++ midterm/blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test import Client
+from .models import Post
 
 
 class PostingTest(TestCase):
@@ -22,6 +23,26 @@ class ModelTest(TestCase):
     def test_model(self):
         t = Post.objects.count()
         self.assertEqual(t, 1)
+
+    def tearDown(self):
+        post = Post.objects.get(title='hello blog')
+        post.delete()
+
+
+class IntegrationTest(TestCase):
+    def test_integration(self):
+        c = Client()
+
+        response = c.get('http://127.0.0.1:8000/blog/')
+        self.assertEqual(response.status_code, 200)
+
+        response = c.post('http://127.0.0.1:8000/blog/post/',
+                          {'title': 'hello blog', 'content': 'blah blah blah'})
+        self.assertEqual(Post.objects.count(), 1)
+
+        post = Post.objects.first()
+        response = c.get('http://127.0.0.1:8000/blog/post/%d/' % post.id)
+        self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
         post = Post.objects.get(title='hello blog')
